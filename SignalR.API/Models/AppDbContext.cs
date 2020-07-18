c6d19ee4af0306e8868e7325402bec40747f8c59
Mehmet Teker <teker.mehmed@gmail.com>
2020-07-18 19:48:55 +0300

    Code first ile DB oluşturma. add-migration start

--- SignalR.API/Models/AppDbContext.cs
+++ SignalR.API/Models/AppDbContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SignalR.API.Models
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
