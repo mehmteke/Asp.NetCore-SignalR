7c151a38aa87417660093cc3f4f8a7110ab61bbc
Mehmet Teker <teker.mehmed@gmail.com>
2020-07-18 16:19:07 +0300

    IHubContext ile Hub dışındaki metotları kullanmak.

---
+++ SignalR.API/Controllers/NotificationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.API.Hubs;
+
+namespace SignalR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> hubContext;
+
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            await hubContext.Clients.All.SendAsync("Notify", $"Arkadaşalr Takım {teamCount} kişi olacak.");
+
+            return  Ok();
+        }
+
+    }
+}
